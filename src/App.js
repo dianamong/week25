94979344c3e1c3810091ef83b8df75be1683ff94
dianamong <saryglardv@gmail.com>
2023-03-09 23:56:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Категория", "Рублей в месяц"],
+  ["Детское", 5000],
+  ["Обучение", 8000],
+  ["Продукты", 4000],
+  ["Счета", 9000],
+  ["Здоровье", 1500],
+];
+
+export const options = {
+  title: "Распределение расходов в марте",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
     </div>
   );
 }
